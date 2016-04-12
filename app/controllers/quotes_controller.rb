e7463bb83eff2f50a32266c6d94e998397cf4012
your <g.lobraico@gmail.com>
2016-04-12 03:09:28 +0200

    Add controllerà

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,34 @@
+class QuotesController < ApplicationController
+  def index
+    @quotes = Quote.all.reorder('created_at DESC')
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new
+
+    if @quote.save
+      redirect_to quotes_path
+    else
+      flash[:error] = "Something went terribly wrong."
+      redirect_to quotes_path
+    end
+  end
+
+  %w(up down).each do |direction|
+    define_method("vote_#{direction}") do
+      @quote = Quote.find(params[:id])
+      @quote.send("vote_#{direction}!".to_sym)
+
+      if @quote.save
+        redirect_to quotes_path
+      else
+        flash[:error] = "Something went terribly wrong."
+        redirect_to quotes_path
+      end
+    end
+  end
+end
